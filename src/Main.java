43c27ac7738de1a8c0f4c06ddb76d0dcd9af9331
Patrick Sigourney <psigourney@gmail.com>
2018-10-21 09:16:01 -0500

    Cleaned up, methodized stuff, added queries

--- src/Main.java
+++ src/Main.java
@@ -18,105 +18,113 @@ public class Main {
         return r.ints(min, (max + 1)).findFirst().getAsInt();
     }
 
+    private static double queryTable(int colA, int colB) throws SQLException{
+        Connection conn2 = DriverManager.getConnection("jdbc:postgresql://localhost/", "postgres", "postgres");
+        Statement st2 = conn2.createStatement();
+        String baseQuery = "SELECT * FROM benchmark";
+        if(colA > 0 || colB > 0){
+            baseQuery += " WHERE ";
+        }
+        if(colA > 0){
+            baseQuery += "columnA = " + Integer.toString(colA);
+            if(colB > 0){
+                baseQuery += " AND " ;
+            }
+        }
+        if(colB > 0){
+            baseQuery += "columnB = " + Integer.toString(colB);
+        }
+
+        long startTime1 = System.nanoTime();
+
+        st2.execute(baseQuery);
+
+        long endTime1 = System.nanoTime();
+        long totalTime1 = endTime1 - startTime1;
+        return (double)totalTime1/1000000000.00;
+    }
+
+    private static double insertRecords(int recordCount, boolean random, boolean indexA, boolean indexB) throws SQLException{
+        Connection conn1 = DriverManager.getConnection("jdbc:postgresql://localhost/", "postgres", "postgres");
+        Statement st1 = conn1.createStatement();
+        st1.execute("DROP TABLE IF EXISTS benchmark");
+        st1.execute("CREATE TABLE IF NOT EXISTS benchmark (theKey numeric PRIMARY KEY, columnA numeric, columnB numeric, filler char(247))");
+        if(indexA){
+            st1.execute("CREATE INDEX idx_benchmark_colA ON benchmark(columnA)");
+        }
+        if(indexB){
+            st1.execute("CREATE INDEX idx_benchmark_colB ON benchmark(columnB)");
+        }
+
+        PreparedStatement pst1 = conn1.prepareStatement("insert into benchmark values (?, ?, ?, ?)");
+
+        Integer[] pkArrayOrdered = new Integer[recordCount];
+        for(int i = 0; i < recordCount; i++){
+            pkArrayOrdered[i] = i+1;
+        }
+
+        Integer[] pkArrayRandom = new Integer[recordCount];
+        for(int i = 0; i < recordCount; i++){
+            pkArrayRandom[i] = i+1;
+        }
+        Collections.shuffle(Arrays.asList(pkArrayRandom));
+
+        String[] fillerArray = new String[5];
+        fillerArray[0] = "dasloghwg";
+        fillerArray[1] = "dahg";
+        fillerArray[2] = "adujghadglhadgljhgwljvhbworuvhbro";
+        fillerArray[3] = "jhgwjuvnwrlvnrewlhglsavnklmvnklwrjgnwrvhrwolrfjbewrogbvrkb";
+        fillerArray[4] = "hdwsjvhbwvbhowrvlknqwrbpoirj3vm423iorenvoermrwvnwr";
+
+        long startTime1 = System.nanoTime();
+
+        for(int i = 0; i < recordCount; i++){
+            pst1.clearParameters();
+
+            if(random) {
+                pst1.setInt(1, pkArrayRandom[i]);  //theKey
+            }
+            else{
+                pst1.setInt(1, pkArrayOrdered[i]);  //theKey
+            }
+            pst1.setInt(2, pkArrayRandom[i]); //columnA
+            pst1.setInt(3, pkArrayRandom[recordCount-1-i]); //columnB
+            pst1.setString(4, fillerArray[getRandomNumberInRange(0,4)]); //filler
+
+            pst1.addBatch();
+            if((i+1)%50000 == 0){
+                pst1.executeBatch();
+                pst1.clearBatch();
+            }
+        }
+
+        conn1.close();
+
+        long endTime1 = System.nanoTime();
+        long totalTime1 = endTime1 - startTime1;
+        return (double)totalTime1/1000000000.00;
+    }
+
 
     public static void main(String[] args) {
+        int n = 1000000;
         try {
+            int[] intArray = new int[] {123456, 234567, 345678, 456789, 567890, 987654, 876543, 765432, 10, 500};
 
-            Connection conn1 = DriverManager.getConnection("jdbc:postgresql://localhost/", "postgres", "postgres");
-            Statement st1 = conn1.createStatement();
-            st1.execute("DROP TABLE IF EXISTS benchmark");
-            st1.execute("CREATE TABLE IF NOT EXISTS benchmark (theKey numeric PRIMARY KEY, columnA numeric, columnB numeric, filler char(247))");
 
-            PreparedStatement pst1 = conn1.prepareStatement("insert into benchmark values (?, ?, ?, ?)");
+            double orderedSeconds = insertRecords(n, false, true, true);
+            System.out.println("Seconds to insert " + n + " ordered records: " + orderedSeconds);
 
-            int n = 5000000; // number of records to insert
-
-            Integer[] pkArrayOrdered = new Integer[n];
-            for(int i = 0; i < n; i++){
-                pkArrayOrdered[i] = i+1;
+            double queryTotal = 0.0;
+            for(int i = 0; i < intArray.length; i++){
+                queryTotal += queryTable(intArray[i], intArray[i]);
             }
+            double queryAvg = queryTotal/intArray.length;
+            System.out.println("Seconds to query " + n + " ordered records with two indexes: " + queryAvg);
 
-            Integer[] pkArrayRandom = new Integer[n];
-            for(int i = 0; i < n; i++){
-                pkArrayRandom[i] = i+1;
-            }
-            Collections.shuffle(Arrays.asList(pkArrayRandom));
+           // double randomSeconds = insertRecords(n, true, true, true);
+           // System.out.println("Seconds to insert " + n + " random records: " + randomSeconds);
 
-
-            String[] fillerArray = new String[5];
-            fillerArray[0] = "dasloghwg";
-            fillerArray[1] = "dahg";
-            fillerArray[2] = "adujghadglhadgljhgwljvhbworuvhbro";
-            fillerArray[3] = "jhgwjuvnwrlvnrewlhglsavnklmvnklwrjgnwrvhrwolrfjbewrogbvrkb";
-            fillerArray[4] = "hdwsjvhbwvbhowrvlknqwrbpoirj3vm423iorenvoermrwvnwr";
-
-
-            long startTime1 = System.nanoTime();
-
-            for(int i = 0; i < n; i++){
-                int rand1 = getRandomNumberInRange(0,4);
-
-                pst1.clearParameters();
-
-                pst1.setInt(1, pkArrayOrdered[i]);  //theKey
-                pst1.setInt(2, pkArrayRandom[i]); //columnA
-                pst1.setInt(3, pkArrayRandom[n-i]); //columnB
-                pst1.setString(4, fillerArray[rand1]); //filler
-
-                pst1.addBatch();
-                if(i%50000 == 0){
-                    pst1.executeBatch();
-                    pst1.clearBatch();
-                }
-
-            }
-
-            conn1.close();
-
-            long endTime1 = System.nanoTime();
-            long totalTime1 = endTime1 - startTime1;
-            double totalSeconds1 = (double)totalTime1/1000000000.00;
-            System.out.println("Total time taken for sorted insert of " + n + " records is " + totalSeconds1 + " seconds");
-
-
-/******************************************************************************************************************/
-
-
-            Connection conn2 = DriverManager.getConnection("jdbc:postgresql://localhost/", "postgres", "postgres");
-            Statement st2 = conn2.createStatement();
-            st2.execute("DROP TABLE IF EXISTS benchmark");
-            st2.execute("CREATE TABLE IF NOT EXISTS benchmark (theKey numeric PRIMARY KEY, columnA numeric, columnB numeric, filler char(247))");
-
-            PreparedStatement pst2 = conn2.prepareStatement("insert into benchmark values (?, ?, ?, ?)");
-
-            long startTime2 = System.nanoTime();
-
-            for(int i = 0; i < n; i++){
-                int rand1 = getRandomNumberInRange(0,4);
-
-                pst2.clearParameters();
-
-                pst2.setInt(1, pkArrayRandom[i]);  //theKey
-                pst2.setInt(2, pkArrayRandom[i]); //columnA
-                pst2.setInt(3, pkArrayRandom[n-i]); //columnB
-                pst2.setString(4, fillerArray[rand1]); //filler
-
-                pst2.addBatch();
-                if(i%50000 == 0){
-                    pst2.executeBatch();
-                    pst2.clearBatch();
-                }
-
-            }
-
-            conn2.close();
-
-            long endTime2 = System.nanoTime();
-            long totalTime2 = endTime2 - startTime2;
-            double totalSeconds2 = (double)totalTime2/1000000000.00;
-            System.out.println("Total time taken for random insert of " + n + " records is " + totalSeconds2 + " seconds");
-
-        }catch(SQLException e){System.out.println("SQL Exception: " + e); System.exit(1);}
-
+        }catch (SQLException e) {System.out.println("SQLException: " + e);}
     }
 }
